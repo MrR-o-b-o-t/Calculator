311d7ad0e7144f785d770db5a29ca3d7f4ac5e57
Sam Kautz <samkautz@yahoo.com>
2020-02-25 22:21:36 -0600

    Add function to display button value to calc display

--- scripts.js
+++ scripts.js
@@ -1,19 +1,28 @@
-// click button 7
-// display 7 to the screen
-// click button 8
-// display 8 to the screen
-// press 'equals' button
-// display 15 to the screen
+// click 7 button
+// get value of button clicked
+// display value to the screen
+// click plus button - no change to screen
+// click 8 button
+// see value of new button displayed to screen
+// press equals
+// see sum of two values displayed to screen
+let num1;
+let num2;
+let plus;
+let minus;
+let devide;
+let decimal;
 
-let num1 = ''
-let num2 = ''
+const numberButtons = document.querySelectorAll('.keys');
 
+numberButtons.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        x = e.target.value
+        document.getElementById('grid-item-display').innerHTML = x;
+    })
+});
 
-function appendInput() {
-    let thisValue = document.querySelectorAll('button').value
-    document.getElementById('grid-item-display').innerHTML = thisValue
-}
-
-function clearInputs() {
+function allClear() {
     document.getElementById('grid-item-display').innerHTML = ''
 }
+
